47c616f365fedc4faa9a572e0665a0dd91bf6349
lqtungntn <57801454+lqtungntn@users.noreply.github.com>
2021-04-22 23:51:16 +0700

    Add files via upload

--- Contact/Contacts.js
+++ Contact/Contacts.js
@@ -1,9 +1,9 @@
 let hpElement = document.querySelector('#BG-hp')
 
-hpElement.addEventListener("mouseover", () => hpElement.style.backgroundColor = 'pink');
+hpElement.addEventListener("mouseover", () => hpElement.style.backgroundColor = '#3767A9');
 hpElement.addEventListener("mouseout", () => hpElement.style.backgroundColor = '#34a853');
 
 let envElement = document.querySelector('#BG-env')
 
-envElement.addEventListener("mouseover", () => envElement.style.backgroundColor = 'pink');
+envElement.addEventListener("mouseover", () => envElement.style.backgroundColor = '#3767A9');
 envElement.addEventListener("mouseout", () => envElement.style.backgroundColor = '#34a853');
