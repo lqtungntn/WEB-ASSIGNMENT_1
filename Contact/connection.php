82faf56157d725edf93ca021f16fa29efb6a5109
lqtungntn <57801454+lqtungntn@users.noreply.github.com>
2021-06-05 22:27:21 +0700

    Add files via upload
    update footer + contact form

---
+++ Contact/connection.php
@@ -0,0 +1,9 @@
+<?php
+$host= "localhost";
+$username = "root";
+$password = "";
+$database = "mercedes";
+    
+// Create connection
+$conn = mysqli_connect($host, $username, $password, $database, "8111");
+?>
