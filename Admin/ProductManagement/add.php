c2b45e9fe3cd8486511dc984b3533c791f607570
Leo16012000 <67458956+Leo16012000@users.noreply.github.com>
2021-06-09 05:57:28 +0700

    All okay in admin

--- Admin/ProductManagement/add.php
+++ Admin/ProductManagement/add.php
@@ -42,26 +42,39 @@ if (empty($_POST["name"])) {
 	}
   }
 
-  //has not validate yet
   $price = $_POST["price"];
-  $detail = $_POST["detail"];
-  $operate = $_POST["operate"];
+  if (empty($_POST["price"])) {
+	$priceErr = "Price is required";
+  } else {
+	if (!is_numeric($price)) {
+	  $priceErr = "Price must be a number";
+	}
+  }
 
+  $detail = $_POST["detail"];
+  if (strlen($detail) < 10){
+	$detailErr ='Detail descripttion must be at least 10 characters in length';
+}
+
+  $operate = $_POST["operate"];
+  if (strlen($operate) < 10){
+	$operateErr ='Operate descripttion must be at least 10 characters in length';
+}
 
 
 
 
 //----------------------------------------------------------------------
 if($nameErr== "" && $imageErr== ""&& $priceErr== ""&& $detailErr== ""&& $operateErr== "") {
-  $sql = "INSERT INTO product (ID,Fullname,Image,Price,Detail,Operate) VALUES ('" . $id . "','" . $name . "','" . $image . "','" . $price . "','" . $detail . "','" . $operate ."')";
+  $sql = "INSERT INTO product (Fullname,Image,Price,Detail,Operate) VALUES ('" . $name . "','" . $image . "'," . $price . ",'" . $detail . "','" . $operate ."')";
   $faq_id = $db_handle->executeInsert($sql);
 	if(!empty($faq_id)) {
-		$sql = "SELECT * from product WHERE id = '$faq_id' ";
+		$sql = "SELECT * from product WHERE ID = '$faq_id' ";
 		$product = $db_handle->runSelectQuery($sql);
 	}
 ?>
 <tr class="table-row" id="table-row-<?php echo $product[0]["ID"]; ?>">
-<td contenteditable="true" id="id" onBlur="addToHiddenField(this,\'id\')" onClick="editRow(this);"><?php echo $id?></td>
+<td><?php echo $faq_id?></td>
 <td contenteditable="true" id="name" onBlur="addToHiddenField(this,\'name\')" onClick="editRow(this);"><?php echo $name?></td>
 <td contenteditable="true" id="image" onBlur="addToHiddenField(this,\'image\')" onClick="editRow(this);"><?php echo $image?></td>
 <td contenteditable="true" id="price" onBlur="addToHiddenField(this,\'price\')" onClick="editRow(this);"><?php echo $price?></td>
@@ -70,8 +83,9 @@ if($nameErr== "" && $imageErr== ""&& $priceErr== ""&& $detailErr== ""&& $operate
 <td><a class="ajax-action-links" onclick="deleteRecord(<?php echo $product[0]["ID"]; ?>);">Delete</a></td>
 </tr>  
 <?php } ?>
-<tr class= "error" class="table-row" id="table-row">
-<td><?php echo $idErr; ?></td>
+<?php if($nameErr!== "" || $imageErr!== "" || $priceErr!== "" || $detailErr!== "" || $operateErr!== "") {?>
+<tr class= "error" class="table-row">
+<td><?php echo "Hello"; ?></td>
 <td><?php echo $nameErr; ?></td>
 <td><?php echo $imageErr; ?></td>
 <td><?php echo $priceErr; ?></td>
@@ -79,3 +93,4 @@ if($nameErr== "" && $imageErr== ""&& $priceErr== ""&& $detailErr== ""&& $operate
 <td><?php echo $operateErr; ?></td>
 <td></td>
 </tr> 
+<?php }?>
